b9c3b611ef8048fd647ce40050f58dcef86b292e
izhengyao <izhengyao@gmail.com>
2026-05-18 13:49:35 +0800

    init

---
+++ src/main/java/io/github/izhengyao/d1/_01_record/PointDemo.java
@@ -0,0 +1,3 @@
+package io.github.izhengyao.d1._01_record;
+
+public record PointDemo(int x, int y) {}
---
+++ src/main/java/io/github/izhengyao/d1/_01_record/RangeWithCompactCtor.java
@@ -0,0 +1,8 @@
+package io.github.izhengyao.d1._01_record;
+public record RangeWithCompactCtor(int low, int high) {
+    public RangeWithCompactCtor {
+        if (low > high) {
+            throw new IllegalArgumentException("low > high");
+        }
+    }
+}
